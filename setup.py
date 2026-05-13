af66c2d72deccfeecbb0a24d486e8fa75f2b3e53
chandrajyothi05 <chandrajyothi.pb@gmail.com>
2026-05-13 13:02:42 +0530

    local package visa setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='visa',
+    version='0.0.0',
+    author="Chandra Fidha",
+    author_email="chandrajyothi.pb@gmail.com",
+    packages=find_packages()   
+
+)
